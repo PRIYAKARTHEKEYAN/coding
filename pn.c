6676bc8fca72c700d1e21e9724c24eca3a48d7e9
PRIYAKARTHEKEYAN <priyadharsinikr.ec13@bitsathy.ac.in>
2016-07-12 13:16:40 +0530

    Create pn.c

---
+++ pn.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int n;
+scanf("%d",&n);
+if(n>=0)
+{
+if(n==0)
+{
+printf("zero");
+else
+printf("negative number");
+}
+else
+printf("positive number");
+return 0;
+}
