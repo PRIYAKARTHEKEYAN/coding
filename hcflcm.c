a2e356fe46d04b444214a877ffcc8a6969026728
PRIYAKARTHEKEYAN <priyadharsinikr.ec13@bitsathy.ac.in>
2016-07-20 20:02:25 +0530

    Update hcflcm.c

--- hcflcm.c
+++ hcflcm.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main()
 {
-int a,b,x,y,t,g,l;
+int a,b,x,y,t,g;
 scanf("%d %d",&x,&y);
 a=x;
 b=y;
@@ -12,8 +12,6 @@ b=a%b;
 a=t;
 }
 g=a;
-l=(x*y)/g;
 printf("gcd of %d and %d is %d",x,y,g);
-printf("lcm of %d and %d is %d",x,y,l);
 return 0;
 }
